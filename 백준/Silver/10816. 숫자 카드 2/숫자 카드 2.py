d1889e412e03d45fbd742bf64d90ea705642510f
김태언 <58901635+Taeeon-kim@users.noreply.github.com>
2025-05-20 09:51:49 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 656 ms, Memory: 134716 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+cardHashTable = {}
+_ = int(input())
+cards = list(map(int,input().split()))
+_ = int(input())
+numbers = list(map(int,input().split()))
+results =[]
+for card in cards:
+    cardHashTable[card] = cardHashTable.get(card, 0) + 1
+
+for number in numbers:
+    results.append(cardHashTable.get(number, 0))
+
+print(" ".join(map(str, results)))
+
+# 시간복잡도 O(N), 공간복잡도 O(N)
