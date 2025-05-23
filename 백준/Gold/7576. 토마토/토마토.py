f2c5e1509c4cefe626211e90354500825752d233
김태언 <58901635+Taeeon-kim@users.noreply.github.com>
2025-05-23 10:00:31 +0900

    [Gold V] Title: 토마토, Time: 1384 ms, Memory: 156400 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,58 @@
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+boxes = []
+maturedTomatos = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+M, N = map(int, input().split())
+states = [[0]* M for _ in range(N)]
+allRipe = True
+for _ in range(N):
+    boxes.append(list(map(int, input().split())))
+
+for i in range(N):
+    for j in range(M):
+        if boxes[i][j] == 1:
+            maturedTomatos.append((i,j))
+        elif boxes[i][j] == 0:
+            allRipe = False
+if len(maturedTomatos) == 0:
+    print(-1)
+elif allRipe: 
+    print(0)
+else :
+    def bfs():
+        queue = deque()
+        count = -1
+        for maturedTomato in maturedTomatos:
+            queue.append(maturedTomato)
+            tmpX, tmpY = maturedTomato
+            states[tmpX][tmpY] = 1
+        for i in range(N):
+            for j in range(M):
+                if boxes[i][j] == -1:
+                    states[i][j] = -1
+        
+        while queue:
+            for _ in range(len(queue)):
+                x,y = queue.popleft()
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+
+                    if 0<= nx < N and 0<= ny < M:
+                        if boxes[nx][ny] == 0 and not states[nx][ny]:
+                            queue.append((nx, ny))
+                            states[nx][ny] = 1
+            count+=1
+        for i in range(N):
+            for j in range(M):
+                if states[i][j] == 0 :
+                    return -1            
+
+        return count
+
+    print(bfs())
