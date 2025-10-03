4f030107d3b38f772a54a207956d74690b0e3f50
김태언 <58901635+Taeeon-kim@users.noreply.github.com>
2025-10-03 09:16:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 88 ms, Memory: 12844 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -6,34 +6,30 @@ public class Main {
         int[] arr = new int[9];
         int[] ans = new int[7];
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) { // O(N)
             arr[i] = sc.nextInt();
         }
 
-        for (int item : arr) { // O(N)
-            sum += item;
-        }
-
+        for (int item : arr) sum += item; // O(N)
+        
+        outer:
         for (int i = 0; i < arr.length; i++) { // 등차수열 O(n(n-1)/2) = O(N^2)
             for (int j = i + 1; j < arr.length; j++) {
                 if (sum - (arr[i] + arr[j]) == 100) {
+                    int idx = 0;
                     for (int k = 0; k < arr.length; k++) {
                         if (k != i && k != j) {
-                            for (int l = 0; l < ans.length; l++) {
-                                if (ans[l] == 0) {
-                                    ans[l] = arr[k];
-                                    break;
-                                }
-                            }
+                            ans[idx++] = arr[k];
                         }
                     }
+                    break outer;
                 }
             }
-        }
+        } // O(N^2) * O(7) = O(N^2 * 7) -> O(N^2)
 
         for (int i = 0; i < ans.length; i++) { // n(n-1)/2 -> O(N^2)
             for (int j = i + 1; j < ans.length; j++) {
-                if(ans[i] > ans[j]){
+                if (ans[i] > ans[j]) {
                     int temp = ans[i];
                     ans[i] = ans[j];
                     ans[j] = temp;
@@ -44,5 +40,5 @@ public class Main {
             System.out.println(value);
         }
     }
-    // O(N^2) + O(N^2) + O(N) + O(N)  = O(2N^2 + 2N) -> O(N^2)
+    // O(N^2) + O(N^2) + O(N) + O(N) + O(N)  = O(2N^2 + 3N) -> O(N^2)
 }
