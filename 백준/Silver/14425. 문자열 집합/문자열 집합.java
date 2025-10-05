be8cb25e0a3a5fe977fc88ab93ad4945b6ea8197
김태언 <58901635+Taeeon-kim@users.noreply.github.com>
2025-10-05 17:46:57 +0900

    [Silver IV] Title: 문자열 집합, Time: 288 ms, Memory: 39020 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,20 +11,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        String[] arr = new String[N];
+        Set<String> set = new HashSet<>();
 
-        for(int i=0;i<N;i++){ // O(N)
-            arr[i] = br.readLine();
+        for (int i = 0; i < N; i++) { // O(N)
+            set.add(br.readLine());
         }
-        Arrays.sort(arr);
-        int resultCount = 0;
-        for(int i=0;i<M;i++){ // O(M)
 
-            int idx = Arrays.binarySearch(arr, br.readLine()); // O(NlogN)
-            if(idx>=0){
+        int resultCount = 0;
+        for (int i = 0; i < M; i++) { // O(M)
+            if (set.contains(br.readLine())) {
                 resultCount++;
             }
-        } // O(M*NlogN)
+        } // O(M) + O(N) = O(M + N) = 20,000
 
         System.out.print(resultCount);
     }
