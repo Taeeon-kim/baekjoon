3634e219b6f04df97766a7181999eaecc95264c6
김태언 <58901635+Taeeon-kim@users.noreply.github.com>
2025-10-12 03:26:55 +0900

    [Silver III] Title: N과 M (6), Time: 88 ms, Memory: 12916 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static int[] numbers;
+    static boolean[] checked;
+    static int[] output;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        numbers = new int[n];
+        checked = new boolean[n];
+        output = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        Arrays.sort(numbers); // 오름차순 정렬
+        perm(0, 0);
+        System.out.print(sb);
+    }
+
+
+
+    static void perm(int depth, int start) {
+        // base case
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        // recursive case
+        for (int i = start; i < n; i++) {
+            if (!checked[i]) {
+                checked[i] = true;
+                output[depth] = numbers[i];
+                perm(depth + 1, i+1);
+                checked[i] = false;
+            }
+        }
+
+    }
+}
